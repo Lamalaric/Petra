2c180ba15bb81454ad1e47b8b9b767a84f8310a1
Bastien <bastien.corgnac@gmail.com>
2021-06-11 00:30:25 +0200

    test

--- admin_connexion.php
+++ admin_connexion.php
@@ -7,15 +7,6 @@
 		header("Location: admin.php");
 	}
 		
-
-	// error_reporting(0);		// Enlève tous les warnings inutiles
-	if ($_POST['login'] == "adminP" && $_POST['password'] == "42021") {			// Vérification des identifiants pour accéder à la page admin
-
-		$_SESSION['login'] = $_POST['login'];
-		$_SESSION['password'] = $_POST['password'];
-
-		header("Location: admin.php");
-	}
 ?>
 
 <body class="container-admin">
@@ -23,7 +14,7 @@
 	<main>
 		<h2>Accès interface administrateur</h2>
 		<div class="container-login">
-			<form method="post" action="">
+			<form method="post" action="connexion.php">
 				<input type="text" name="login" placeholder="login" required>
 				<input type="password" name="password" placeholder="mot de passe" required>
 				<input type="submit" name="submit" value="Connexion">
